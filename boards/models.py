f38310480b90dbab851ac20979c561c2d3a238a2
ravipal86 <ravi.pal@fafadiatech.com>
2018-01-12 20:38:33 +0530

    created forms and reusable template

--- boards/models.py
+++ boards/models.py
@@ -11,7 +11,7 @@ class Board(models.Model):
 
 
 class Topic(models.Model):
-    subject = models.TextField(max_length=255)
+    subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics')
     starter = models.ForeignKey(User, related_name='topics')
